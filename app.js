ec4a4d0b49c2385100e08d06c1f3f3afe91ae02e
ShaunBillows <billows.shaun@gmail.com>
2022-08-17 01:16:03 +0100

    formatting

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ require('dotenv').config()
 const finnhub = require('finnhub');
 
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = process.env.API_KEY // <== add API_KEY env var
+api_key.apiKey = process.env.API_KEY // add API_KEY here
 const finnhubClient = new finnhub.DefaultApi()
 
 const getPrice = (stock) => {
         const currentTime = Math.round(new Date().getTime() / 1000);
-        finnhubClient.stockCandles(stock,"D" , currentTime-60, currentTime, (error, data, response) => {
+        finnhubClient.stockCandles(stock, "D" , currentTime-60, currentTime, (error, data, response) => {
             console.log(data.c) 
     });
 }
 
 const getPrices = (stocks) => {
     stocks.map( (stock, i) => {
-        setTimeout(function() {
+        setTimeout( () => {
             getPrice(stock)
-          }, 1000/31 * i)
+          }, 1000/31 * i) // limit : 30 req/sec
         }
     )
 }
